8c78fc9e0b8bb8c559b2906baaef2a05cf8b96d9
Aryaanil26 <157900533+Aryaanil26@users.noreply.github.com>
2024-05-06 11:23:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const form = document.getElementById('form')
+const form = document.getElementById( 'form' )
+
 const  contactlist = document.getElementById('contactlist')
-form.addEventListener('submit',handelSubmission)
+
+form.addEventListener('submit', handelSubmission)
+
+
 
 function handelSubmission(event){
     event.preventDefault()
     
-    const pictureInput =form['picture']
-    const picture =picture.Input.value
+    const pictureInput = form['picture']
+    const picture = pictureInput.value;
 
-    const nameInput =form['name']
-    const name =name.Input.value
+    const nameInput = form['name']
+    const name = nameInput.value;
 
-    const phoneInput =form['phone']
-    const phone =phone.Input.value
+    const phoneInput = form['phone']
+    const phone = phoneInput.value;
 
-    const li =document.createElement('li')
+    const li = document.createElement('li');
 
-    const img = document.createElement('img')
-    img.src =picture
-    li.appendChild(img)
+    const img = document.createElement('img');
+    img.src = picture
+    li.appendChild(img);
 
-    const division = document.createElemen('div')
-    li.appendChild(division)
+    const division = document.createElement('div');
+    li.appendChild(division);
 
-    const h3 = document.createElement('h3')
+    const h3 = document.createElement('h3');
     h3.innerHTML= name
     division.appendChild('h3')
     
-    const span = document.createElement('span')
-    span.innerHTML = phone
-    division.appendChild('span')
+    const span = document.createElement('span');
+    span.innerHTML = phone;
+    division.appendChild('span');
 
     li.appendChild('division')
     contactlist.appendChild(li)
